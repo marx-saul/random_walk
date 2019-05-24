1106ad62fbe3210edbb14e0372e5884272d06078
marx-saul <47049698+marx-saul@users.noreply.github.com>
2019-05-24 22:52:45 +0900

    Update main.py
    make a bar graph of the time of steps for the player to bunkrupt

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import copy
 
 INF = -1
-Going = 0;
+Going = 0
 PlayerLose = 1
 MasterLose = 2
+MAX_TIME = 2 * 10**3
+MAX_SIM  = 10**3
 
 class Game:
     # money, money, probability of player winning
@@ -32,8 +35,9 @@ class Game:
             else: return Going
     
 
-def simulate(game, max_time = 100000):
-    trans = [game.player] ; step_num = max_time
+# plot the transition of the money player has
+def simulate1(game, max_time = MAX_TIME, flag = False):
+    trans = [game.player]
     
     for i in range(0, max_time):
        result = game.step()
@@ -43,16 +47,38 @@ def simulate(game, max_time = 100000):
        trans.append(game.player)
        #print(game.player)
        
-       if (result != Going): step_num = i+1; break
-       
-    plt.plot(np.array(range(0, step_num+1)), np.array(trans))
-    plt.xlabel("step") ; plt.ylabel("player's money") ; plt.title("random walk")
-    plt.show()
+       if (result != Going): break
     
-    return trans
+    if flag:
+        plt.plot( np.array(range(0, step_num+1)), np.array(trans) )
+        plt.xlabel("step") ; plt.ylabel("player's money") ; plt.title("random walk")
+        plt.show()
+    
+    return len(trans)-1
     
 
+def simulate2(game, sim_num = MAX_SIM, max_time = MAX_TIME):
+    game_ = copy.deepcopy(game)
+    times = [0] * (max_time+1)
+    
+    for i in range(0, sim_num):
+        step_num = 0;
+        
+        for j in range(1, max_time+1):
+            step_num += 1
+            result = game_.step()
+            if result != Going: break
+            
+        times[step_num] += 1;
+        game_ = copy.deepcopy(game);
+    
+    plt.bar( np.array(range(0, max_time+1)), np.array(times) )
+    plt.xlabel("step") ; plt.ylabel("frequency") ; plt.title("number of steps to bankrupt")
+    plt.show()
+        
+
 if __name__ == "__main__":
-    game = Game(player=20, master=80, p=0.50)
-    trans = simulate(game)
-    print("time = ", len(trans))
+    game = Game(player=20, master=INF, p=0.50)
+    #time = simulate1(game, flag=True)
+    #print("time = ", time)
+    simulate2(game)
