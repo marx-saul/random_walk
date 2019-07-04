f7191c0e298e65cd7c1da9e022d596cd28040d37
marx-saul <47049698+marx-saul@users.noreply.github.com>
2019-07-04 22:39:21 +0900

    Update simulate12.py

--- simulate12.py
+++ simulate12.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import copy
-import sys
+from math import *
+from statistics import mean, median,variance,stdev
 
 INF = -1
 Going = 0
@@ -10,6 +11,7 @@ PlayerLose = 1
 MasterLose = 2
 MAX_TIME = 2 * 10**3
 MAX_SIM  = 2 * 10**3
+    
 
 class Game:
     # money, money, probability of player winning
@@ -34,9 +36,8 @@ class Game:
             if self.player == 0: return PlayerLose
             elif self.master == 0: return MasterLose
             else: return Going
-    
 
-def simulate12(game, trial_num = 2000):
+def simulate(game, trial_num = 500):
     results = [0] * 101;
     
     for n in range(0, trial_num):
@@ -46,10 +47,36 @@ def simulate12(game, trial_num = 2000):
             if result != Going: break
         results[game_.player] += 1
     
-    plt.bar( np.array(range(0, 101)), np.array(results) )
-    plt.xlabel("player's money") ; plt.ylabel("times") ; plt.title("after 80 bets")
-    plt.show()
+    return results
+
+def simulate12(game, trial_num = 500):
+    t_n = 100
+    results = [[0 for i in range(t_n)] for j in range(101)]
+
+    #combs = calc(80)
     
+    for n in range(0, t_n):
+        result = simulate(game, trial_num)
+        #print(result)
+        #print('########')
+        for i in range(0, 101):
+            results[i][n] = result[i]
+            #print(results[i][n])
+    
+    #print(results)
+    
+    mean_result = [0] * 101
+    stdev_result = [0] * 101
+    for i in range(0, 101):
+        mean_result[i] = mean(results[i])
+        stdev_result[i] = stdev(results[i])
+
+    print(mean_result)
+    print(stdev_result)
+
+    plt.bar( np.array(range(0, 101)), np.array(mean_result), yerr = np.array(stdev_result) )
+    plt.show()
+
 
 # ().py simulation_type player_money master_money probability
 if __name__ == "__main__":
